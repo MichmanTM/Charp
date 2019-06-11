1a061820477708311dfdaa1299ad647553898cd5
MichmanTM <buten91@mail.ru>
2019-06-11 23:59:36 +0300

    дз к четвертому уроку 1 и 4 задача(сделать 2 задучу мне не хватило знаний и времени )

---
+++ HomeWork4/homework4/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework4
+{
+    class Program
+    {
+        static void Main(string[] args)                                  
+        {
+            MyArray myArray = new MyArray(20, -10_000, 10_000);            //задаем параметры метода MyArray  
+            Console.WriteLine( myArray.ToString()   );                    //выводим перемееную 
+            myArray.Divisibility3();                                       // выполняем действие с переменной при помощи метода
+            Console.ReadLine();
+        }
+    }
+    public class MyArray
+    {
+        private  int[] b;                                               //обьевляем масив
+
+        public MyArray(int n, int min, int max)                          //создаем метод
+        {
+            b = new int[n];                                             //инициализируем масив
+            Random random = new Random();                               //создаем переменную random и инициализируем ее
+            for (int i = 0; i < n; i++)                                       
+                b[i] = random.Next(min, max);                                //заполняем масив
+
+        }
+        public int Divisibility3()                                                              //создаем метод проверки числа и и находим пары чисел
+        {
+            int count = 0;
+            for (int i = 0; i<b.Length - 1; i++)
+            {
+                if (b[i] % 3 == 0 || b[i+1] % 3 == 0)
+                    count++;
+                Console.WriteLine("пара чисел:  {0} и {1}" , b[i], b[i+1]);
+
+            }
+            Console.WriteLine($"количество пар:" + count);
+            return count;
+        }
+        override public string ToString()
+        {                                                                                       
+            string stringarray = "";
+            foreach (int x in b)
+            
+                stringarray = stringarray + x + "  ";
+            
+
+            return stringarray;
+        }
+    }
+           
+        
+    
+}
---
+++ HomeWork4/homework44/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace homework44
+{
+    class MyArray
+    {       
+
+        string[] a;
+        public MyArray(string filename)
+        {
+            StreamReader sw = new StreamReader(@"D:\file.txt");
+
+            a = new string[2];
+            for (int l = 0; l < 2; l++)
+            {
+                a[l] = sw.ReadLine();
+                                                                     //  записываем текстовый файл в масив             
+            }
+            // Console.WriteLine($"a[0]={a[0]},a[1]= {a[1]}");                                             
+
+            sw.Close();
+            
+        
+            string[] mass = new string[2];                                                  //создаем новый масив  
+            int i = 0;
+            do
+            {
+                Console.WriteLine($" введите логин");                                   //запрашиваем у пользователя логин и пароль
+                mass[0] = Console.ReadLine();
+                Console.WriteLine($"введите пароль");
+                mass[1] = Console.ReadLine();
+                i++;
+                if (i >= 3)                                  
+                {
+                    Console.WriteLine($" вы исчерпали попытки, попробуйте чуть позднее ");
+                    return;
+                }
+                Console.WriteLine($"попробуйте еще раз ");
+            }
+            while (mass[0] != a[0] && mass[1] != a[1]);                                          //прверяем совпадает ли логин и пароль
+            // while (mass != a && mass != a) ;
+
+
+
+            Console.WriteLine("добро пожаловать ");
+        }
+
+        
+        
+
+
+    }
+     class Program
+    {
+        static void Main(string[] args)
+        {
+
+            StreamWriter sw = new StreamWriter("D:\\file.txt");     //создаем текстовый файл м логом и паролем
+            sw.WriteLine("root");
+            sw.WriteLine("GeekBrains");                                             
+            sw.Close();
+                                                                             // Console.ReadLine();
+
+
+            MyArray a = new MyArray(@"D:\file.txt");
+         
+            Console.ReadKey();
+            
+            
+               
+               
+                
+            
+        }
+    }
+}
